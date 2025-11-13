d365b9d567e641a4ac6b772b058c48e46e65a817
Sascha Bergenås <sascha.bergenas@hotmail.com>
2025-11-13 22:39:00 +0100

    fixed shiny

--- script2.js
+++ script2.js
@@ -1,8 +1,16 @@
 const pokemonId = document.getElementById("pokemonId");
-const btn = document.getElementById("btn");
 const pokemonSprites = document.getElementById("pokemonSprites");
 const pokemonNo = document.getElementById("pokemonNo");
+const imgElement = document.getElementById("pokemonSprites");
+const imgShinyElement = document.getElementById("pokemonShinySprites");
 const pokemonName = document.getElementById("pokemonName");
+const container = document.querySelector(".pokemonContainer");
+const shinyButton = document.createElement("button");
+shinyButton.textContent = "☆";
+shinyButton.id = "shinyButton";
+const btn = document
+  .getElementById("btn")
+  .addEventListener("click", getPokemonByNumber);
 
 function getPokemonByNumber() {
   const config = {
@@ -10,28 +18,44 @@ function getPokemonByNumber() {
       Accept: "application/json",
     },
   };
-  const pokemonId = document.getElementById("pokemonId").value;
+  const pokemonId = document.getElementById("pokemonId");
 
-  fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId, config)
+  fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId.value, config)
     .then((response) => response.json())
     .then((data) => {
       const pokemonSprites = data.sprites.front_default;
-      const pokemonName = data.name;
-      const pokemonNumber = data.id;
-
-      const imgElement = document.getElementById("pokemonSprites");
-      const nameElement = document.getElementById("pokemonName");
-      const pokemonNo = document.getElementById("pokemonNo");
+      const pokemonShinySprites = data.sprites.front_shiny;
 
       imgElement.src = pokemonSprites;
+      imgShinyElement.src = pokemonShinySprites;
       imgElement.style.display = "block";
 
-      nameElement.textContent = pokemonName;
-      nameElement.style.display = "block";
-      pokemonNo.textContent = `#${pokemonNumber}`;
+      pokemonName.textContent = data.name;
+      pokemonName.style.display = "block";
+      pokemonNo.textContent = `#${data.id}`;
       pokemonNo.style.display = "block";
+      console.log(data);
+      pokemonId.value = "";
+      container.appendChild(shinyButton);
     })
     .catch((err) => {
       console.error("något gick fel", err);
     });
 }
+
+function toggleShiny() {
+  if (imgElement.style.display === "block") {
+    imgElement.style.display = "none";
+    shinyButton.style.color = "yellow";
+  } else {
+    imgElement.style.display = "block";
+    shinyButton.style.color = "black";
+  }
+  if (imgShinyElement.style.display === "block") {
+    imgShinyElement.style.display = "none";
+  } else {
+    imgShinyElement.style.display = "block";
+  }
+}
+
+shinyButton.addEventListener("click", toggleShiny);
