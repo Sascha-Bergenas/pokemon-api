6c74e0714448e194171985c608a2c019ae850042
Sascha Bergenås <sascha.bergenas@hotmail.com>
2025-11-17 22:19:37 +0100

    pokemon by name

--- pokemonByName/main.js
+++ pokemonByName/main.js
@@ -1,18 +1,18 @@
 const container = document.querySelector(".container");
-const sprite = document.querySelector("#pokemonSprite");
+const spriteImage = document.querySelector("#pokemonSprites");
+const spriteShinyImage = document.querySelector("#pokemonShinySprites");
 
 async function pokemonApi() {
   const pokemonName = document.querySelector("input").value.toLocaleLowerCase();
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
   try {
-    removeOldList();
     const response = await fetch(url);
 
     if (!response.ok) {
       throw new Error("something went sideways");
     }
     const data = await response.json();
-    renderPokemonTypes(data);
+
     console.log(data);
 
     return data;
@@ -26,10 +26,15 @@ const renderPokemonTypes = (data) => {
   typeList.id = "typeList";
   data.types.forEach((item) => {
     const typeListItem = document.createElement("li");
-    typeListItem.textContent = item.type.name;
+    const typeImg = document.createElement("img");
+    // typeListItem.textContent = item.type.name;
     typeList.appendChild(typeListItem);
+    const typeNumber = item.type.url.split("/type/").pop().split("/")[0];
+    typeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-iv/heartgold-soulsilver/${typeNumber}.png`;
+    typeListItem.appendChild(typeImg);
   });
-
+  typeOutput.textContent = "Type:";
+  container.appendChild(typeOutput);
   container.appendChild(typeList);
 };
 const removeOldList = () => {
@@ -39,14 +44,27 @@ const removeOldList = () => {
   }
 };
 
-const inputPokemon = document.createElement("input");
-container.appendChild(inputPokemon);
+const pokemonSprites = (data) => {
+  const pokemonSprites = data.sprites.other.showdown.front_default;
+  const pokemonShinySprites = data.sprites.other.showdown.front_shiny;
+  spriteImage.src = pokemonSprites;
+  spriteShinyImage.src = pokemonShinySprites;
+  spriteImage.style.display = "block";
+  spriteShinyImage.style.display = "block";
+};
 
+const inputPokemon = document.createElement("input");
+inputPokemon.id = "inputen";
 const searchButton = document.createElement("button");
-container.appendChild(searchButton);
-searchButton.textContent = "search";
-searchButton.addEventListener("click", () => {
+const typeOutput = document.createElement("p");
+container.prepend(searchButton);
+searchButton.textContent = "Find That Pokémon!";
+container.prepend(inputPokemon);
+
+searchButton.addEventListener("click", async () => {
   removeOldList();
-  pokemonApi();
+  const data = await pokemonApi();
+  pokemonSprites(data);
+  renderPokemonTypes(data);
   inputPokemon.value = "";
 });
